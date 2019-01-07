a35adef8df5b46d507eaf340cda33be6b2b9fca9
Michael McCool <michael.mccool@intel.com>
2019-01-07 15:45:41 +0900

    restructure

--- solarpower.ino
+++ solarpower.ino
@@ -6,6 +6,11 @@
 #include <Adafruit_INA219.h>
 #include <Adafruit_BMP280.h>
 
+// Configuration
+const bool pretty = true; // pretty-print JSON data
+const float battery_capacity_Wh = 1400;
+
+// Sensors
 Adafruit_BMP280 bme; 
 DHT12 dht12; 
 Adafruit_INA219 ina219_A(0x40); // solar panel input
@@ -16,38 +21,39 @@ void setup(void)
 {
   M5.begin();
   Wire.begin();
-  Wire.setClock(100000); // Std Mode
+  Wire.setClock(100000); // Std I2C Speed (Fast does not work reliably with BMP280)
   
   M5.Lcd.begin();
   M5.Lcd.setBrightness(10);
+  
   M5.Lcd.fillScreen( BLACK );
   M5.Lcd.setCursor(0, 0);
-  M5.Lcd.setTextColor( WHITE );  
   M5.Lcd.setTextSize(2);
+  M5.Lcd.setTextColor( YELLOW ); 
 
-  M5.Lcd.fillScreen( BLACK );
-  M5.Lcd.setCursor(0, 0);
-  M5.Lcd.println("Power/ENV Meter");
+  M5.Lcd.println("Solar");
+  M5.Lcd.println("Power");
+  M5.Lcd.println("Monitor");
 
   delay(3000);
  
   Serial.begin(115200);
   while (!Serial) {
-      // pause until serial console opens
-      delay(1);
+    // pause until serial console opens
+    delay(1);
   }
   M5.Lcd.fillScreen( BLACK );
 
   {
     if (!bme.begin(0x76)) {  
       Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+      //while (1);
     }
-    //while (1);
   }
 
   uint32_t currentFrequency;
     
-  Serial.println("Hello!");
+  Serial.println("Solar Power Monitor");
   
   // Initialize the INA219s.
   // By default the initialization will use the largest range (32V, 2A).
@@ -56,20 +62,98 @@ void setup(void)
   ina219_C.begin();
   
   // Slightly lower 32V, 1A range (higher precision on amps):
-  ina219_A.setCalibration_32V_1A();
-  ina219_B.setCalibration_32V_1A();
+  ina219_A.setCalibration_32V_1A();  // Panel max 21V
+  ina219_B.setCalibration_32V_1A();  // Charger nominal 16V, but may be slightly higher
   // Lower 16V, 400mA range (higher precision on volts and amps):
-  ina219_C.setCalibration_16V_400mA();
+  ina219_C.setCalibration_16V_400mA(); // Battery max 12.6V
 
-  Serial.println("Measuring voltages and currents with 3x INA219");
+  Serial.println("Measuring voltages and currents with INA219s");
   Serial.println("Measuring temperature and humidity with DHT12");
   Serial.println("Measuring pressure with BMP280");
+}
+
+// Channel configuration
+const unsigned int N_CHANNELS = 3;
+// correction factors
+const float cf[N_CHANNELS] = {
+  7.0*1.13,
+  7.0*1.8,
+  7.0*1.1 
+};
+// names (JSON, Display)
+const char* cn[N_CHANNELS][2] = {
+  { "panel",  "Panel" },
+  { "charge", "Charge" },
+  { "output", "Output" }
+};
+
+// Last data read or computed
+float bus_V[N_CHANNELS];
+float shunt_mV[N_CHANNELS];
+float load_V[N_CHANNELS];
+float current_A[N_CHANNELS];
+float power_W[N_CHANNELS];
+
+float temperature;
+float humidity;
+float pressure;
+
+float cs;  // charge state; estimate % of capacity
+float cp;  // charge state; estimated Wh
+
+// Estimate percent-of-charge from voltage.
+// Very rough! Just linear function between min and max voltages
+// Also, this will be inaccurate when the battery is being charged,
+// and it also does not take into account the voltage drop due to 
+// loading.
+float voltage_to_cs(float v) {
+  const float L = 10.00; // voltage for 0% charge (about 3.3V per cell)
+  const float H = 12.50; // voltage for 100% charge ("overcharge > 100% possible)
+  float p = (v - L)/(H - L);
+  if (p < 0.0) p = 0.0;
+  return p;
+}
+
+// Read data from sensors, compute derived values,
+// and store in above variables
+void read_sensors () {
+  // voltage, current, and power on channel A/0 (solar panel input)
+  shunt_mV[0] =  ina219_A.getShuntVoltage_mV();
+  bus_V[0] =     ina219_A.getBusVoltage_V();
+  current_A[0] = cf[0]*ina219_A.getCurrent_mA()/1000.0;
+  power_W[0] =   cf[0]*ina219_A.getPower_mW()/1000.0;
+  load_V[0] =    bus_V[0] + shunt_mV[0]/1000.0;
+
+  // voltage, current, and power on channel B/1 (backup charger) 
+  shunt_mV[1] =  ina219_B.getShuntVoltage_mV();
+  bus_V[1] =     ina219_B.getBusVoltage_V();
+  current_A[1] = cf[1]*ina219_B.getCurrent_mA()/1000.0;
+  power_W[1] =   cf[1]*ina219_B.getPower_mW()/1000.0;
+  load_V[1] =    bus_V[1] + shunt_mV[1]/1000.0;
+ 
+  // voltage, current, and power on channel C/2 (load)
+  shunt_mV[2] =  ina219_C.getShuntVoltage_mV();
+  bus_V[2] =     ina219_C.getBusVoltage_V();
+  current_A[2] = cf[2]*ina219_C.getCurrent_mA()/1000.0;
+  power_W[2] =   cf[2]*ina219_C.getPower_mW()/1000.0;
+  load_V[2] =    bus_V[2] + shunt_mV[2]/1000.0;
+
+  // environmental sensors
+  temperature = dht12.readTemperature();
+  humidity = dht12.readHumidity();
+  pressure = bme.readPressure();
+
+  // charge state estimates
+  cs = voltage_to_cs(bus_V[2]); // load voltage is also battery voltage
+  cp = battery_capacity_Wh*cs;
 }
 
 #define PRINT(A)         {M5.Lcd.print(A);}
 #define PRINTLN(A)       {M5.Lcd.println(A);}
 #define PRINT_FLT(A,D)   {M5.Lcd.print(A,D);}
 
+// Print a float right-justified in a field of with w with d digits
+// to the right of the decimal (if 0, no decimal point printed)
 void print_flt(float f, int w, int d) {
   bool nf = (f < 0.0);
   float af = nf ? -f : f;
@@ -90,94 +174,68 @@ void print_flt(float f, int w, int d) {
   PRINT_FLT(af,d);
 }
 
-// Estimate percent-of-charge from voltage.
-// Very rough! Just linear function between min and max voltages
-// Also, this will be inaccurate when the battery is being charged,
-// and it also does not take into account the voltage drop due to 
-// loading.
-float voltage_to_cs(float v) {
-  float c = 3; // number of cells
-  float L = 3.3*c; // per-cell voltage for 0% charge
-  float H = 4.2*c; // per-cell voltage for 100% charge
-  float p = (v - L)/(H - L);
-  if (p < 0.0) p = 0.0;
-  if (p > 1.0) p = 1.0;
-  return p;
-}
+// record index
+int rec_index = 0;
 
-void loop(void) 
-{
+// display mode
+const unsigned int DP_DETAILED = 0;
+const unsigned int DP_CHARGE_PERCENT = 1;
+const unsigned int N_DP = 2;
+unsigned int disp_mode = DP_DETAILED;
+
+// display detailed data on LCD
+void disp_detailed() {
+  if (0 == rec_index) return; // no data yet
+
+  // Blank display, initialize options
+  M5.Lcd.setTextSize(2);
   M5.Lcd.fillScreen( BLACK );
   M5.Lcd.setCursor(0, 0);
+  int cw = 6; // column width
 
-  const float cf_A = 7.0*1.13;
-  float shunt_A =   ina219_A.getShuntVoltage_mV();
-  float bus_A =     ina219_A.getBusVoltage_V();
-  float current_A = cf_A*ina219_A.getCurrent_mA()/1000.0;
-  float power_A =   cf_A*ina219_A.getPower_mW()/1000.0;
-  float load_A =    bus_A + shunt_A/1000.0;
-
-  const float cf_B = 7.0*1.8;
-  float shunt_B =   ina219_B.getShuntVoltage_mV();
-  float bus_B =     ina219_B.getBusVoltage_V();
-  float current_B = cf_B*ina219_B.getCurrent_mA()/1000.0;
-  float power_B =   cf_B*ina219_B.getPower_mW()/1000.0;
-  float load_B =    bus_B + shunt_B/1000.0;
-
-  const float cf_C = 7.0*1.1;
-  float shunt_C =   ina219_C.getShuntVoltage_mV();
-  float bus_C =     ina219_C.getBusVoltage_V();
-  float current_C = cf_C*ina219_C.getCurrent_mA()/1000.0;
-  float power_C =   cf_C*ina219_C.getPower_mW()/1000.0;
-  float load_C =    bus_C + shunt_C/1000.0;
-
-  // Display Detailed Data on LCD
-  int cw = 6;
+  // Power Flow Data
   M5.Lcd.setTextColor( YELLOW ); 
   PRINTLN("POWER");
   M5.Lcd.setTextColor( WHITE );
   PRINTLN("    Panel Charge Output");
   PRINT("B: "); 
-    print_flt(bus_A,cw,2);     
+    print_flt(bus_V[0],cw,2);     
     PRINT(" ");
-    print_flt(bus_B,cw,2);     
+    print_flt(bus_V[1],cw,2);     
     PRINT(" ");
-    print_flt(bus_C,cw,2);
+    print_flt(bus_V[2],cw,2);
     PRINTLN(" V");
   PRINT("S: ");
-    print_flt(shunt_A,cw,2);   
+    print_flt(shunt_mV[0],cw,2);   
     PRINT(" ");
-    print_flt(shunt_B,cw,2);   
+    print_flt(shunt_mV[1],cw,2);   
     PRINT(" ");   
-    print_flt(shunt_C,cw,2);   
+    print_flt(shunt_mV[2],cw,2);   
     PRINTLN(" mV");
   PRINT("L: ");
-    print_flt(load_A,cw,2);    
+    print_flt(load_V[0],cw,2);    
     PRINT(" ");
-    print_flt(load_B,cw,2);    
+    print_flt(load_V[1],cw,2);    
     PRINT(" ");   
-    print_flt(load_C,cw,2);    
+    print_flt(load_V[2],cw,2);    
     PRINTLN(" V");
   PRINT("C: "); 
-    print_flt(current_A,cw,2); 
+    print_flt(current_A[0],cw,2); 
     PRINT(" ");
-    print_flt(current_B,cw,2); 
+    print_flt(current_A[1],cw,2); 
     PRINT(" ");    
-    print_flt(current_C,cw,2); 
+    print_flt(current_A[2],cw,2); 
     PRINTLN(" A");
   PRINT("P: ");
-    print_flt(power_A,cw,2);   
+    print_flt(power_W[0],cw,2);   
     PRINT(" ");
-    print_flt(power_B,cw,2);   
+    print_flt(power_W[1],cw,2);   
     PRINT(" ");   
-    print_flt(power_C,cw,2);   
+    print_flt(power_W[2],cw,2);   
     PRINTLN(" W");
   PRINTLN("");
 
-  float temperature = dht12.readTemperature();
-  float humidity = dht12.readHumidity();
-  float pressure = bme.readPressure();
-
+  // Environmental Data
   M5.Lcd.setTextColor( GREEN ); 
   PRINTLN("ENVIRONMENT");
   M5.Lcd.setTextColor( WHITE );
@@ -192,50 +250,204 @@ void loop(void)
     PRINTLN(" Pa");
   PRINTLN("");  
 
-  float cs = voltage_to_cs(bus_C);
-  float cp = 1400*cs;
-
+  // Charge Status (Estimated)
   M5.Lcd.setTextColor( RED ); 
   PRINT("STATUS: ");
-  M5.Lcd.setTextColor( WHITE); 
+  M5.Lcd.setTextColor( WHITE ); 
   print_flt(100*cs,3,0); PRINT(" %  ");
-  print_flt(cp,4,0); PRINT(" Wh");
+  print_flt(cp,4,0); PRINT(" Wh"); 
+}
 
+void pi(int w) {
+  if (pretty) while (w > 0) {
+    Serial.print(" ");
+    w--;
+  }
+}
+void ps() {
+  pi(1);
+}
+void pe(const char* text = "") {
+  if (pretty) {
+    Serial.println(text);
+  } else {
+    Serial.print(text);
+  }
+}
+void send_rec(int w = 0) {
+  pi(w);
+  Serial.print("\"id\":");              
+  ps();
+  Serial.print(rec_index);
+}
+void send_channel(unsigned int ch, bool rec = true, int w = 0) {
+  if (ch >= N_CHANNELS) return;
+
+  Serial.print("{"); 
+  pe();
+
+  if (rec) {
+    send_rec(w+2);    
+    pe(","); 
+  }
+
+  pi(w+2); 
+  Serial.print("\"bus_V\":"); 
+  ps(); 
+  Serial.print(bus_V[ch],2); 
+  pe(","); 
+
+  pi(w+2); 
+  Serial.print("\"shunt_mV\":"); 
+  ps(); 
+  Serial.print(shunt_mV[ch],2); 
+  pe(","); 
+
+  pi(w+2); 
+  Serial.print("\"load_V\":"); 
+  ps(); 
+  Serial.print(load_V[ch],2); 
+  pe(","); 
+
+  pi(w+2); 
+  Serial.print("\"current_A\":"); 
+  ps(); 
+  Serial.print(current_A[ch],2); 
+  pe(","); 
+
+  pi(w+2); 
+  Serial.print("\"power_W\":"); 
+  ps(); 
+  Serial.print(power_W[ch],2); 
+  Serial.print(","); 
+  pe();
+
+  pi(w);
+  Serial.print("}");
+}
+void send_power(int w = 0, bool rec = false) {
+  Serial.print("{");
+  pe();
+
+  if (rec) {
+    send_rec(w+2);    
+    Serial.print(",");
+    pe();
+  }
+
+  pi(w+2); 
+  Serial.print("\"");
+  Serial.print(cn[0][0]);
+  Serial.print("\":");
+  ps();
+  send_channel(0,false,w+2);
+  pe(",");
+
+  pi(w+2);
+  Serial.print("\"");
+  Serial.print(cn[1][0]);
+  Serial.print("\":");
+  ps();
+  send_channel(1,false,w+2);
+  pe(",");
+
+  pi(w+2);
+  Serial.print("\"");
+  Serial.print(cn[2][0]);
+  Serial.print("\":");
+  ps();
+  send_channel(2,false,w+2);
+  pe();
+
+  pi(w);
+  Serial.print("}");
+}
+void send_charge(int w = 0, bool rec = false) {
+  Serial.print("{");
+  pe();
+
+  if (rec) {
+    send_rec(w+2);    
+    Serial.print(",");
+    pe();
+  }
+
+  pi(w+2);
+  Serial.print("\"charge_pcnt\":"); 
+  ps();
+  Serial.print(100*cs,2); 
+  pe(",");
+
+  Serial.print(  "    \"charge_Wh\": ");     Serial.println(cp,2);     
+  pi(w+2);
+  Serial.print("\"charge_Wh\":"); 
+  ps();
+  Serial.print(cp,2); 
+
+  pi(w);
+  Serial.print("}");
+}
+void send_env(int w = 0, bool rec = false) {
+  Serial.println("{");
+
+  pi(w+2);
+  Serial.print("\"temperature_C\":"); 
+  ps();
+  Serial.print(temperature,2); 
+  pe(",");
+
+  pi(w+2);
+  Serial.print("\"humidity_pcnt\":");
+  ps();
+  Serial.print(humidity,2);
+  pe(",");
+
+  pi(w+2);
+  Serial.print("\"pressure_Pa\": ");
+  ps();
+  Serial.print(pressure,2);  
+  pe();
+
+  pi(w);
+  Serial.print("}");
+}
+
+void send_all() {
   // Report Detailed Data via Serial Formatted as JSON
   Serial.println("{");
-  Serial.println("  \"power\": {");
-  Serial.println("    \"panel\": {"); 
-  Serial.print(  "      \"bus_V\": ");       Serial.print(bus_A,2);       Serial.println(",");
-  Serial.print(  "      \"shunt_mV\": ");    Serial.print(shunt_A,2);     Serial.println(",");
-  Serial.print(  "      \"load_V\": ");      Serial.print(load_A,2);      Serial.println(",");
-  Serial.print(  "      \"current_A\": ");   Serial.print(current_A,2);   Serial.println(",");
-  Serial.print(  "      \"power_W\": ");     Serial.println(power_A,2);
-  Serial.println("    },");
-  Serial.println("    \"charge\": {"); 
-  Serial.print(  "      \"bus_V\": ");       Serial.print(bus_B,2);       Serial.println(",");
-  Serial.print(  "      \"shunt_mV\": ");    Serial.print(shunt_B,2);     Serial.println(",");
-  Serial.print(  "      \"load_V\": ");      Serial.print(load_B,2);      Serial.println(",");
-  Serial.print(  "      \"current_A\": ");   Serial.print(current_B,2);   Serial.println(",");
-  Serial.print(  "      \"power_W\": ");     Serial.println(power_B,2);
-  Serial.println("    },");
-  Serial.println("    \"output\": {"); 
-  Serial.print(  "      \"bus_V\": ");       Serial.print(bus_C,2);       Serial.println(",");
-  Serial.print(  "      \"shunt_mV\": ");    Serial.print(shunt_C,2);     Serial.println(",");
-  Serial.print(  "      \"load_V\": ");      Serial.print(load_C,2);      Serial.println(",");
-  Serial.print(  "      \"current_A\": ");   Serial.print(current_C,2);   Serial.println(",");
-  Serial.print(  "      \"power_W\": ");     Serial.println(power_C,2);
-  Serial.println("    }");
-  Serial.println("  },");
-  Serial.println("  \"status\": {");
-  Serial.print(  "    \"charge_pcnt\": ");   Serial.print(100*cs,2);      Serial.println(",");
-  Serial.print(  "    \"charge_Wh\": ");     Serial.println(cp,2);     
-  Serial.println("  },");
-  Serial.println("  \"environment\": {");
-  Serial.print(  "    \"temperature_C\": "); Serial.print(temperature,2); Serial.println(",");
-  Serial.print(  "    \"humidity_pcnt\": "); Serial.print(humidity,2);    Serial.println(",");
-  Serial.print(  "    \"pressure_Pa\": ");   Serial.println(pressure,2);  
-  Serial.println("  }");
-  Serial.println("};");  // use ";" as record separator
+  send_rec(2); pe(",");
+  pi(2); Serial.print("\"power\":"); ps();
+  send_power(2,false); pe(",");
+  pi(2); Serial.print("\"environment\":"); ps();
+  send_env(2,false); pe(",");
+  pi(2); Serial.print("\"status\":"); ps();
+  send_env(2,false); pe();
+  pe("};"); // use ";" as record separator
+}
+
+void loop(void) 
+{
+  read_sensors();
+  rec_index++;
+
+  // If A button pressed, rotate display modes
+
+
+  // Display data using selected mode
+  switch (disp_mode) {
+     default:
+     case DP_DETAILED:
+       //disp_detailed();
+       break;
+     case DP_CHARGE_PERCENT:
+       // disp_charge_percent();
+       break;
+  };
+
+  // Read commands from serial input; as long as there are
+  // some, execute them until buffer is empty
+ 
+  send_all();
 
   delay(5000);
 }
